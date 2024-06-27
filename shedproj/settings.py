eb4d13287681c77e4375487a8523258c04c5dc20
Anar <anar@rocky.anar.com>
2024-06-26 19:55:04 -0700

    added db and migrations

--- shedproj/settings.py
+++ shedproj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'simplepwd',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'shedproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates/simplepwd"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
