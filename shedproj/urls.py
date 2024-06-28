7113fa8a24fa454d070c984382afb7f522f6fed1
Anar <anar@rocky.anar.com>
2024-06-27 21:18:36 -0700

    added urls.py to app

--- shedproj/urls.py
+++ shedproj/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from simplepwd import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', views.testview),
+    path('',views.index,name='index'),
+    path('simplepwd/', include('simplepwd.urls')),
 ]
---
+++ simplepwd/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from simplepwd import views
+
+
+urlpatterns = [
+    path('')
+]
