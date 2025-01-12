01e2f32de1e82afd6d9d2cb8d43c1aa1de52452c
Anar <anar@rocky.anar.com>
2025-01-11 23:12:20 -0800

    fixed logout

--- shedproj/settings.py
+++ shedproj/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL ='http://192.168.1.110:32027/static'
+STATIC_URL ='http://192.168.1.110:32027/static/'
 
 #STATIC_ROOT = BASE_DIR / 'static'
 
